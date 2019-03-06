d259cc79d9f2c9d74fa73e5368b6bb7f73a30626
aprasa32 <aprasa32@in.ibm.com>
2019-03-06 09:17:27 +0530

    Task 3

---
+++ src/main/java/com/stackroute/task/Main.java
@@ -0,0 +1,17 @@
+package com.stackroute.task;
+
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        applicationContext.registerShutdownHook();
+        Movie movie = (Movie)applicationContext.getBean("movie");
+        System.out.println(movie.getActor());
+    }
+}
