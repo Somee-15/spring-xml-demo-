0e74506f2717d6c1f3c19ea7f635f4ccee1f5dd3
aprasa32 <aprasa32@in.ibm.com>
2019-03-06 09:15:23 +0530

    Task 2

---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,20 @@
+package com.stackroute.domain;
+
+public class Movie {
+    Actor actor;
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    public Movie() {
+    }
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+}
---
+++ src/main/java/com/stackroute/task/Main.java
@@ -0,0 +1,17 @@
+package com.stackroute.task;
+
+import com.stackroute.domain.Movie;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie = (Movie)applicationContext.getBean("movie");
+        System.out.println(movie.getActor());
+
+        Movie movie1 = (Movie)applicationContext.getBean("movie");
+        System.out.println(movie.getActor());
+        System.out.println(movie==movie1);
+    }
+}
